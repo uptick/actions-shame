205cb9945e562a3559b4aebe1f87064616dc4262
william chu <william.chu@uptickhq.com>
2022-11-24 10:38:21 +1100

    Moar debugging

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-var child_process = require('child_process');
+const child_process = require('child_process');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-var child = child_process.spawnSync("ls", ["dist"], { encoding : 'utf8' });
-console.log("Process finished.");
-if(child.error) {
-    console.log("ERROR: ",child.error);
+  // `who-to-greet` input defined in action metadata file
+const command= core.getInput('command');
+console.log(`Command: ${command}`)
+
+
+function cmd(program, args) {
+    var child = child_process.spawnSync(program, args, { encoding : 'utf8' });
+    console.log(`Process ${program} finished.`);
+    if(child.error) {
+        console.log("ERROR: ",child.error);
+    }
+    console.log("stdout: ",child.stdout);
+    console.log("stderr: ",child.stderr);
+    console.log("exist code: ",child.status);
 }
-console.log("stdout: ",child.stdout);
-console.log("stderr: ",child.stderr);
-console.log("exist code: ",child.status);
 
-var child = child_process.spawnSync("ls", [""], { encoding : 'utf8' });
-console.log("Process finished.");
-if(child.error) {
-    console.log("ERROR: ",child.error);
-}
-console.log("stdout: ",child.stdout);
-console.log("stderr: ",child.stderr);
-console.log("exist code: ",child.status);
-
-
-var child = child_process.spawnSync("python3", ["src/main.py", cmd], { encoding : 'utf8' });
-console.log("Process finished.");
-if(child.error) {
-    console.log("ERROR: ",child.error);
-}
-console.log("stdout: ",child.stdout);
-console.log("stderr: ",child.stderr);
-console.log("exist code: ",child.status);
+cmd("ls", ["$(pwd)"])
+cmd("ls", ["."])
+cmd("ls", ["dist"])
+cmd("find", ["."])
