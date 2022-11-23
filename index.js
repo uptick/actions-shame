c17ebd0a8b7daea77665e816d0312c545ef075cb
william chu <william.chu@uptickhq.com>
2022-11-24 10:40:48 +1100

    moar

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 const child_process = require('child_process');
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-  // `who-to-greet` input defined in action metadata file
-const command= core.getInput('command');
-console.log(`Command: ${command}`)
-
 
 function cmd(program, args) {
     var child = child_process.spawnSync(program, args, { encoding : 'utf8' });
@@ -22,3 +15,4 @@ cmd("ls", ["$(pwd)"])
 cmd("ls", ["."])
 cmd("ls", ["dist"])
 cmd("find", ["."])
+cmd("bash", ["-c", "env"])
