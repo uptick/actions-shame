4eab4361daa3e17733b957916cde89d7a692b393
william chu <william.chu@uptickhq.com>
2022-11-24 10:22:19 +1100

    Add shame command

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var exec = require('child_process').exec;
+var child_process = require('child_process');
+const cmd = "shame"
 
-exec('ls')
+var child = child_process.spawnSync("python3", ["src/main.py", cmd], { encoding : 'utf8' });
+console.log("Process finished.");
+if(child.error) {
+    console.log("ERROR: ",child.error);
+}
+console.log("stdout: ",child.stdout);
+console.log("stderr: ",child.stderr);
+console.log("exist code: ",child.status);
