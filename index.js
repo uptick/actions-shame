64222f622d65889e57dfd1803a9607db1af9edb8
william chu <william.chu@uptickhq.com>
2022-11-24 10:29:55 +1100

    readd index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var child_process = require('child_process');
+
+var child = child_process.spawnSync("ls", ["dist"], { encoding : 'utf8' });
+console.log("Process finished.");
+if(child.error) {
+    console.log("ERROR: ",child.error);
+}
+console.log("stdout: ",child.stdout);
+console.log("stderr: ",child.stderr);
+console.log("exist code: ",child.status);
+
+var child = child_process.spawnSync("ls", [""], { encoding : 'utf8' });
+console.log("Process finished.");
+if(child.error) {
+    console.log("ERROR: ",child.error);
+}
+console.log("stdout: ",child.stdout);
+console.log("stderr: ",child.stderr);
+console.log("exist code: ",child.status);
+
+
+var child = child_process.spawnSync("python3", ["src/main.py", cmd], { encoding : 'utf8' });
+console.log("Process finished.");
+if(child.error) {
+    console.log("ERROR: ",child.error);
+}
+console.log("stdout: ",child.stdout);
+console.log("stderr: ",child.stderr);
+console.log("exist code: ",child.status);
